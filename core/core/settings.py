30c21984c161b5724205ea93309581349d6ee255
yassin <yassronaldo33@gmail.com>
2025-05-22 00:01:31 +0300

    initial commit

--- core/core/settings.py
+++ core/core/settings.py
@@ -80,17 +80,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 from dotenv import load_dotenv
+import dj_database_url
 import os
 load_dotenv()
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT", "5432"),
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
