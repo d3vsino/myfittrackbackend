011b1a9658de4d1d2ee63ce0be871c9587a3e9d0
Devsino <emraldshop@gmail.com>
2025-05-23 17:15:17 +0300

    Update urls.py

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
-from .views import  AiView, CalorieLogViewSet, UserProfileView, RegisterView, ChatHistoryView, FoodSearchAPIView, MacroFromImageView
+from .views import  AiView, CalorieLogViewSet, UserProfileView, RegisterView, ChatHistoryView, MacroFromImageView
 from rest_framework.routers import DefaultRouter
 
 calorie_router = DefaultRouter()
@@ -9,7 +9,6 @@ calorie_router.register(r'calorie-logs', CalorieLogViewSet, basename='calorie-lo
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('ai/', AiView.as_view()),
-    path('api/foods/', FoodSearchAPIView.as_view(), name='food-search'),
     path('ai/chat-history/', ChatHistoryView.as_view()),
     path('token/obtain/', TokenObtainPairView.as_view()),
     path('analyze-meal/', MacroFromImageView.as_view(), name='analyze-meal'),
